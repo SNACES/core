25fc9489639cda799e0a2cee2bcb8bc604e0a0e5
JimmyZhengyj <yjzheng98@sina.com>
2020-10-01 21:18:40 +0800

    add functions to merge word frequency table

---
+++ src/merge/merge_wf.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+
+# input database
+new_db_name = "WordFrequency-Test"
+orig_db_name = "WordFrequency"
+
+#input dada collection
+collection_name = "GlobalWordCount"
+
+#location in MongoDB
+client = MongoClient('mongodb://localhost:27017')
+
+#Combine WordFrequency
+new_db = client[new_db_name]
+orig_db = client[orig_db_name]
+
+new_collection = new_db[collection_name]
+orig_collection = orig_db[collection_name]
+
+new_wf = new_collection.find_one()
+orig_wf = orig_collection.find_one()
+
+#merge
+merge_wf = merge(new_wf, orig_wf)
+
+#insert
+new_collection.remove({})
+new_collection.insert_one(merge_wf)
+
+
+
+
+def merge(dict1, dict2):
+    for item in dict2.keys():
+        if item in dict1:
+            dict1[item] += dict2[item]
+        else:
+            dict1[item] = dict2[item]
+    return dict1
