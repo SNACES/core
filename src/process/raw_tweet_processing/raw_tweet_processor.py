eb401f8bb8185c26424fae2739ac06b48440b921
Matthew Verreault <matthew@postalgia.ink>
2020-10-02 22:02:44 -0400

    Updated documentation for raw tweet processing

--- src/process/raw_tweet_processing/raw_tweet_processor.py
+++ src/process/raw_tweet_processing/raw_tweet_processor.py
@@ -1,29 +1,30 @@
 import nltk
 import re
 import datetime
+from src.model.raw_tweet import RawTweet
+# from src.model.processed_tweet import ProcessedTweet
 
 class RawTweetProcessor():
     def __init__(self):
         nltk.download('stopwords')
 
-    def gen_processed_global_tweets(self, tweet_getter, tweet_setter, processed_tweet_setter):
+    def gen_processed_global_tweets(self, tweet_getter, tweet_setter, processed_tweet_setter) -> None:
         """
-        Assume that the input dao contains a random stream of tweets. 
-        The common format is: [tweet text].
-        Return and store processed global tweets.
-        Update global tweet database to reflect processed tweet state.
+        Processes tweets retrieved from a getter, and stores them using the given getters
+
+        @param tweet_getter dao to retrieve tweets
+        @param tweet_setter dao to update the state of raw tweets to indicate they have been processed
+        @param processed_tweet_setter dao to store the processed tweets
         """
-        
+
         global_tweet_list = tweet_getter.get_global_tweets()
         processed_global_tweet_list = list(map(self._process_tweet_text, global_tweet_list))
         processed_tweet_setter.store_global_processed_tweets(processed_global_tweet_list)
         tweet_setter.update_global_tweet_state()
 
-        return processed_global_tweet_list
-
-    def gen_processed_user_tweets(self, tweet_getter, tweet_setter, processed_tweet_setter):
+    def gen_processed_user_tweets(self, tweet_getter, tweet_setter, processed_tweet_setter) -> None:
         """
-        Assume that the input dao contains tweets associated with users. 
+        Assume that the input dao contains tweets associated with users.
         The common format is: {user: [tweet text]}.
         Return and store processed user tweets.
         Update user tweet database to reflect processed tweet state.
@@ -34,17 +35,23 @@ class RawTweetProcessor():
 
         for user in user_to_tweets:
             tweet_list = user_to_tweets[user]
+            # processed_tweet_list = map(self._process_tweet_text, tweet_list)
             processed_tweet_list = map(self._process_tweet_text, tweet_list)
             user_to_processed_tweet_list[user] = processed_tweet_list
 
         processed_tweet_setter.store_user_processed_tweets(user_to_processed_tweet_list)
         tweet_setter.update_user_tweet_state()
 
-        return user_to_processed_tweet_list
+    def _process_tweet_text(self, tweet: RawTweet): # -> ProcessedTweet:
+        """
+        Processes a given tweet
 
-    def _process_tweet_text(self, text):
+        @param tweet the raw, unprocessed tweet
+        @return the processed tweet
+        """
+        text = tweet.get_text()
         text = text.lower()
-        
+
         # Filter links, numbers, and emojis
         text = re.sub(r"\bhttps:\S*\b", "", text)
         text = re.sub(r"\b\d*\b", "", text)
@@ -58,7 +65,7 @@ class RawTweetProcessor():
                 processed_text_list[i] = ''
 
         processed_text_list = list(filter(lambda x: x != '', processed_text_list))
-        
+
         # Run stemming: it's important to run this first before stop words for cases such as that's
         sno = nltk.stem.SnowballStemmer('english')
         processed_text_list = [sno.stem(word) for word in processed_text_list]
@@ -69,7 +76,7 @@ class RawTweetProcessor():
         for word in stopwords:
             if word in processed_text_list:
                 # extract
-                while (processed_text_list.count(word)): 
+                while (processed_text_list.count(word)):
                     processed_text_list.remove(word)
 
         return processed_text_list
