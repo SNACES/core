347963d7c2ebd530df5f47e895b03f8c2246627c
Matthew Verreault <matthew@postalgia.ink>
2020-10-09 19:36:38 -0400

    Added Model for a Tweet

---
+++ src/model/tweet.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+from typing import Union, Optional
+import json
+
+class Tweet:
+    """
+    A class to represent a tweet
+    """
+    def __init__(self, id: int, user_id: int, created_at: str, text: str,
+            lang: str, retweet_id: Optional[int],
+            retweet_user_id: Optional[int], quote_id: Optional[int],
+            quote_user_id: Optional[int]) -> Tweet:
+        """
+        Default constructor for a tweet
+
+        @param id the tweet's unique id
+        @param user_id the id of the user who tweeted the tweet
+        @param created_at a date string representation of the date the tweet
+            was created
+        @param text the text of the tweet
+        @param lang the language of the tweet
+        @param retweet_id if the tweet is a retweet, this is the id of the
+            original tweet, otherwise it is None
+        @param retweet_user_id if the tweet is a retweet, this is the user
+            id of the original tweet's tweeter, otherwise it is None
+        @param quote_id if the tweet is a quote, this is the id of the
+            original tweet, otherwise it is None
+        @param quote_user_id if the tweet is a quote, this is the user id of the
+            original tweet's tweeter, otherwise it is None
+        """
+        self.id = id
+        self.user_id = user_id
+        self.created_at = created_at
+        self.text = text
+        self.lang = lang
+        self.retweet_id = retweet_id
+        self.retweet_user_id = retweet_user_id
+        self.quote_id = quote_id
+        self.quote_user_id = quote_user_id
+
+    def toJSON(self: Tweet) -> str:
+        """
+        Returns a json corresponding to the given tweet object
+
+        @return a json representation of the tweet
+        """
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True,
+            indent=4)
+
+    def fromJSON(json_in: str) -> Tweet:
+        """
+        Given a json representation of a tweet, returns the tweet object
+
+        @param json_in the json to convert to a Tweet
+
+        @return the Tweet object
+        """
+        obj = json.loads(json_in)
+        tweet = Tweet(
+            obj.get("id"),
+            obj.get("user_id"),
+            obj.get("created_at"),
+            obj.get("text"),
+            obj.get("lang"),
+            obj.get("retweet_id"),
+            obj.get("retweet_user_id"),
+            obj.get("quote_id"),
+            obj.get("quote_user_id"))
+
+        return tweet
+
+    def fromTweepyJSON(json_in: Dict) -> Tweet:
+        """
+        Given a json representation of a tweet returned by Tweepy, returns the
+        tweet object
+
+        @param json_in the json to convert to a Tweet
+
+        @return the Tweet object
+        """
+        id = json_in.get("id")
+        user_id = json_in.get("user").get("id")
+        created_at = json_in.get("created_at")
+        text = json_in.get("text")
+        lang = json_in.get("lang")
+        retweet_id = json_in.get("retweeted_status").get("id") \
+            if json_in.get("retweeted_status") is not None \
+            else None
+        retweet_user_id = json_in.get("retweeted_status").get("user").get("id") \
+            if json_in.get("retweeted_status") is not None \
+            else None
+        quote_id = json_in.get("quoted_status").get("id") \
+            if json_in.get("quoted_status") is not None \
+            else None
+        quote_user_id = json_in.get("quoted_status").get("user").get("id") \
+            if json_in.get("retweeted_status") is not None \
+            else None
+
+        return Tweet(id, user_id, created_at, text, lang, retweet_id,
+            retweet_user_id, quote_id, quote_user_id)
+
+
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other) -> bool:
+        return not self.__eq__(other)
