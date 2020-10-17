9d61cc684051dda3e8463a481bfb2c34aa3d442f
JimmyZhengyj <yjzheng98@sina.com>
2020-10-17 11:22:43 +0800

    new collecitons for wf

--- src/shared/mongo.py
+++ src/shared/mongo.py
@@ -1,11 +1,17 @@
 from pymongo import MongoClient
+import datetime
 
-def get_collection_from_config(mongo_config):
+def get_collection_from_config(mongo_config, gen_new_collection = False):
     db_location = mongo_config['location'] 
-    db_name = mongo_config['datastore-name']
+    
+    if gen_new_collection:
+        db_name = mongo_config['datastore-name' + str(datetime.date.today())]
+    else:
+        db_name = mongo_config['datastore-name']
     
     client = MongoClient(db_location)
     db = client[db_name]
     collection_name = mongo_config['collection-name']
 
     return db[collection_name]
+
