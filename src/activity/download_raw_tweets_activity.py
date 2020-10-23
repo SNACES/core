49d3901b664d0cc328c4924665269256d07dae88
Matthew Verreault <matthew@postalgia.ink>
2020-10-22 22:35:23 -0400

    Added activity and daemon script for raw tweet process

---
+++ src/activity/download_raw_tweets_activity.py
@@ -0,0 +1,24 @@
+from src.shared.mongo import get_collection_from_config
+from src.dao.twitter.tweepy_twitter_dao import TweepyTwitterGetter
+from src.dao.raw_tweet.setter.mongo_raw_tweet_setter import MongoRawTweetSetter
+from src.process.download.tweet_downloader import TwitterTweetDownloader
+from typing import Dict
+
+class DownloadUserTweetsActivity():
+    def __init__(self, config: Dict):
+        self.tweet_downloader = None
+        self.configure(config)
+
+    def configure(self, config: Dict):
+        if config is not None:
+            tweepy_getter = TweepyTwitterGetter()
+            raw_tweet_setter = MongoRawTweetSetter()
+
+            collection = get_collection_from_config(config)
+
+            raw_tweet_setter.set_tweet_collection(collection)
+
+            self.tweet_downloader = TwitterTweetDownloader(tweepy_getter, raw_tweet_setter)
+
+    def download_random_tweet(self):
+        self.tweet_downloader.download_random_tweet()
