fe4b8315fd9558ea15d42c9ad245a20a93aed210
Matthew Verreault <matthew@postalgia.ink>
2020-11-27 20:35:09 -0500

    Added user downloader

---
+++ src/process/download/user_downloader.py
@@ -0,0 +1,19 @@
+from src.dao.twitter.twitter_dao import TwitterGetter
+from src.dao.user.setter.user_setter import UserSetter
+
+class TwitterUserDownloader():
+    """
+    Downloads a twitter User
+    """
+
+    def __init__(self, twitter_getter: TwitterGetter, user_setter: UserSetter):
+        self.twitter_getter = twitter_getter
+        self.user_setter = user_setter
+
+    def download_user_by_screen_name(self, screen_name: str):
+        user = self.twitter_getter.get_user_by_screen_name(screen_name)
+        self.user_setter.store_user(user)
+
+    def download_user_by_id(self, user_id: int):
+        user = self.twitter_getter.get_user_by_id(user_id)
+        self.user_setter.store_user(user)
