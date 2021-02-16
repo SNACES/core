01c856a426480fd3211bef14e3509f3eaed15708
Matthew Verreault <matthewverreault@yahoo.com>
2021-02-16 14:20:24 -0500

    Added error logging to script

--- src/scripts/detect_core.py
+++ src/scripts/detect_core.py
@@ -2,16 +2,23 @@ import argparse
 import time
 from src.dependencies.injector import Injector
 from src.shared.utils import get_project_root
+from src.shared.logger_factory import LoggerFactory
+
+log = LoggerFactory.logger(__name__)
 
 DEFAULT_PATH = str(get_project_root()) + "/src/scripts/config/detect_core_config.yaml"
 
 
 def detect_core(name: int, path=DEFAULT_PATH):
-    injector = Injector.get_injector_from_file(path)
-    process_module = injector.get_process_module()
+    try:
+        injector = Injector.get_injector_from_file(path)
+        process_module = injector.get_process_module()
 
-    core_detector = process_module.get_core_detector()
-    core_detector.detect_core_by_screen_name(name)
+        core_detector = process_module.get_core_detector()
+        core_detector.detect_core_by_screen_name(name)
+    except Error as e:
+        log.error(e)
+        exit()
 
 
 if __name__ == "__main__":
