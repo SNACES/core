621192d76caea76b936a3ce386c7ad28c16eabf6
h-meng8 <60942572+h-meng8@users.noreply.github.com>
2021-06-28 20:21:01 -0400

    Fixed method call to clean_friends_global

--- src/process/data_cleaning/extended_friends_cleaner.py
+++ src/process/data_cleaning/extended_friends_cleaner.py
@@ -13,11 +13,12 @@ class ExtendedFriendsCleaner():
         self.consumption_ranker = consumption_ranker
         self.retweets_ranker = retweets_ranker
 
-    def clean_friends(self, user_id, tweet_threshold=50, follower_threshold=50,
+    def clean_friends(self, user_id, tweet_threshold=50, follower_threshold=50, friend_threshold=0,
                       bot_threshold=0, local_follower=0,
                       local_following=10, consumption_threshold=20, production_threshold=50):
         friends_list = self.user_friends_getter.get_user_friends_ids(user_id)
-        clean_friends_1 = self.clean_friends_global(user_id, friends_list, tweet_threshold, follower_threshold, bot_threshold)
+        clean_friends_1 = self.clean_friends_global(user_id, friends_list, tweet_threshold, follower_threshold,
+                                                    friend_threshold, bot_threshold)
         return self.clean_friends_local(user_id, clean_friends_1, local_follower, local_following)
 
     def clean_friends_global(self, user_id, friends_list, tweet_threshold, follower_threshold, friend_threshold,
