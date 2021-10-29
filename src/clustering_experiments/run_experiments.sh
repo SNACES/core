b2a9e6a033c636415e6438c71eee2e8c214d95c6
aksh-n <akshatn77@gmail.com>
2021-10-28 23:07:26 -0400

    Add script to run experiments

---
+++ src/clustering_experiments/run_experiments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while getopts n: option
+do
+    case "${option}" in
+        n) number_of_experiments=${OPTARG};;
+    esac
+done
+
+for ((i = 0; i <= $number_of_experiments; i++));
+do
+    python ./src/clustering_experiments/compare_clustering_algorithms.py
+done
