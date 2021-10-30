e7609415cb3856fd3ea7aec84030da1a1280b0fa
aksh-n <akshatn77@gmail.com>
2021-10-30 16:08:01 -0400

    Add graph and aggregate results python scripts.

--- src/clustering_experiments/run_experiments.sh
+++ src/clustering_experiments/run_experiments.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-while getopts n: option
+while getopts n:f: option
 do
     case "${option}" in
         n) number_of_experiments=${OPTARG};;
+        f) file=${OPTARG};;
     esac
 done
 
 for ((i = 0; i < $number_of_experiments; i++));
 do
-    python ./src/clustering_experiments/compare_clustering_algorithms.py
+    python ./src/clustering_experiments/$file
 done
