ca9d0bc7159500494facff1a7dcf3dbc8cf88a77
aksh-n <akshatn77@gmail.com>
2021-11-07 10:27:20 -0500

    Calculate and display top users over runs in graphs

--- src/clustering_experiments/aggregate_results.py
+++ src/clustering_experiments/aggregate_results.py
@@ -1,4 +1,5 @@
 from compare_clustering_algorithms import get_clusters, threshold_clusters
+from ranking_users_in_clusters import rank_users
 
 
 def write_cluster_sizes(initial_user: str, clusters, refined_clusters):
@@ -9,6 +10,38 @@ def write_cluster_sizes(initial_user: str, clusters, refined_clusters):
         non_discarded_refined_clusters = threshold_clusters(refined_clusters)
         f.write(str(len(non_discarded_refined_clusters)) + "\n")
 
+
+def write_cluster_ranking(initial_user: str, clusters, refined_clusters):
+    """Write number of clusters to text files given clusters and refined clusters of a particular experiment run."""
+    refined_clusters = threshold_clusters(refined_clusters)  # Discard the very small clusters
+    with open(f"./src/clustering_experiments/data/{initial_user}_rankings/clustering_production_ranking.txt", "a") as p:
+        with open(f"./src/clustering_experiments/data/{initial_user}_rankings/clustering_consumption_ranking.txt", "a") as c:        
+            ranking_users_all_clusters = [rank_users(initial_user, cluster) for cluster in clusters]
+            p.write("-"*15 + "\n")
+            c.write("-"*15 + "\n")
+            for i, ranking_of_one_cluster in enumerate(ranking_users_all_clusters):
+                p.write(f"*Cluster {i}*\n")
+                c.write(f"*Cluster {i}*\n")
+                for prod_user in ranking_of_one_cluster[0]:
+                    p.write(prod_user + "\n")
+                for con_user in ranking_of_one_cluster[1]:
+                    c.write(con_user + "\n")
+
+    with open(f"./src/clustering_experiments/data/{initial_user}_rankings/refined_clustering_production_ranking.txt", "a") as p:
+        with open(f"./src/clustering_experiments/data/{initial_user}_rankings/refined_clustering_consumption_ranking.txt", "a") as c:        
+            ranking_users_all_clusters = [rank_users(initial_user, refined_cluster) for refined_cluster in refined_clusters]
+            p.write("-"*15 + "\n")
+            c.write("-"*15 + "\n")
+            for i, ranking_of_one_cluster in enumerate(ranking_users_all_clusters):
+                p.write(f"*Refined Cluster {i}*\n")
+                c.write(f"*Refined Cluster {i}*\n")
+                for prod_user in ranking_of_one_cluster[0]:
+                    p.write(prod_user + "\n")
+                for con_user in ranking_of_one_cluster[1]:
+                    c.write(con_user + "\n")
+
+
 if __name__ == "__main__":
     clusters, refined_clusters = get_clusters("david_madras")
-    write_cluster_sizes("david_madras", clusters, refined_clusters)
+    # write_cluster_sizes("david_madras", clusters, refined_clusters)
+    write_cluster_ranking("david_madras", clusters, refined_clusters)
--- src/clustering_experiments/graph_comparison.py
+++ src/clustering_experiments/graph_comparison.py
@@ -2,21 +2,108 @@ from collections import Counter
 import matplotlib.pyplot as plt
 
 
-def compare_number_clusters_user(initial_user: str):
+def compare_number_clusters_user(initial_user: str, runs: int=10):
     with open(f"./src/clustering_experiments/data/{initial_user}_number_of_clusters.txt", "r") as f:
-        number_of_clusters_list = [int(num) for num in f.readlines()]
+        number_of_clusters_list = [int(num) for num in f.readlines()][:runs]
     with open(f"./src/clustering_experiments/data/{initial_user}_number_of_refined_clusters.txt", "r") as f:
-        number_of_refined_clusters_list = [int(num) for num in f.readlines()]
-    compare_number_clusters(number_of_clusters_list)
-    compare_number_clusters(number_of_refined_clusters_list)
+        number_of_refined_clusters_list = [int(num) for num in f.readlines()][:runs]
+    compare_number_clusters(number_of_clusters_list, colour="lightblue")
+    compare_number_clusters(number_of_refined_clusters_list, refined=True, colour="darkblue")
 
-def compare_number_clusters(number_of_clusters_list: list):
+def compare_number_clusters(number_of_clusters_list: list, refined=False, colour="blue"):
     """Plots the number of clusters across experiment runs."""
     number_of_clusters_count = Counter(number_of_clusters_list).most_common()
     number_of_clusters, count = [num[0] for num in number_of_clusters_count], [num[1] for num in number_of_clusters_count]
-    plt.bar(number_of_clusters, count)
+
+    if refined:
+        title = f"Number of Refined Clusters over {len(number_of_clusters_list)} runs"
+    else:
+        title = f"Number of Clusters over {len(number_of_clusters_list)} runs"
+
+    fig, ax = plt.subplots()
+    ax.bar(number_of_clusters, count, color=colour)
+    ax.set_xlabel("Number of Clusters")
+    ax.set_ylabel("Count across runs")
+    ax.set_xticks(range(1, max(number_of_clusters) + 1))
+    ax.set_yticks(range(0, len(number_of_clusters_list) + 1, len(number_of_clusters_list) // 5))
+    ax.set_title(title)
+
+    plt.show()
+
+def compare_top_users_user(initial_user: str, num_clusters: int=2, num_refined_clusters: int=3, runs: int=10):
+    """Compares the top 10 users for each cluster across runs with have num_clusters each run."""
+    def helper(refined=False, prod=True):
+        n_clusters = [num_clusters, num_refined_clusters][refined]
+        if refined:
+            refined = "refined_"
+        else:
+            refined = ""
+        if prod:
+            utility = "production"
+        else:
+            utility = "consumption"
+        with open(f"./src/clustering_experiments/data/{initial_user}_rankings/{refined}clustering_{utility}_ranking.txt", "r") as f:
+            lines = f.readlines()
+            final_list = []  # List of top users for each cluster across each run
+            top_users_run = []
+            top_users_cluster = []
+            for line in lines:
+                line = line.strip()
+                if line[0] == "-":
+                    top_users_run.append(top_users_cluster)
+                    final_list.append(top_users_run)
+                    top_users_run = []
+                    top_users_cluster = []
+                elif line[0] == "*":
+                    if top_users_cluster != []:
+                        top_users_run.append(top_users_cluster)
+                        top_users_cluster = []
+                else:
+                    top_users_cluster.append(line)
+        return [sublist for sublist in final_list if len(sublist) == n_clusters][:runs]
+    
+    l = helper()
+    list_users_prod_clusters = [[top_users_run[i] for top_users_run in l] for i in range(num_clusters)]  # List of top users across runs for each cluster
+    for i, list_cluster in enumerate(list_users_prod_clusters):
+        compare_top_users(list_cluster, title=f"Top 10 Users of Cluster {i} by Production Utility over {runs} runs")
+    
+    l = helper(prod=False)
+    list_users_con_clusters = [[top_users_run[i] for top_users_run in l] for i in range(num_clusters)]  # List of top users across runs for each cluster
+    for i, list_cluster in enumerate(list_users_con_clusters):
+        compare_top_users(list_cluster, title=f"Top 10 Users of Cluster {i} by Consumption Utility over {runs} runs")
+    
+    l = helper(refined=True)
+    list_users_prod_refined_clusters = [[top_users_run[i] for top_users_run in l] for i in range(num_refined_clusters)]  # List of top users across runs for each cluster
+    for i, list_cluster in enumerate(list_users_prod_refined_clusters):
+        compare_top_users(list_cluster, title=f"Top 10 Users of Refined Cluster {i} by Production Utility over {runs} runs")
+    
+    l = helper(refined=True, prod=False)
+    list_users_prod_refined_clusters = [[top_users_run[i] for top_users_run in l] for i in range(num_refined_clusters)]  # List of top users across runs for each cluster
+    for i, list_cluster in enumerate(list_users_prod_refined_clusters):
+        compare_top_users(list_cluster, title=f"Top 10 Users of Refined Cluster {i} by Consumption Utility over {runs} runs")
+
+    
+
+                
+
+def compare_top_users(list_of_top_users_across_runs, title: str, n: int=10, colour="blue"):
+    """Compares the top n users across runs"""
+    top_users_list = []
+    for l in list_of_top_users_across_runs:
+        top_users_list.extend(l)
+    top_users_count = Counter(top_users_list).most_common(n)
+    top_users, count = [user[0] for user in top_users_count], [user[1] for user in top_users_count]
+
+    fix, ax = plt.subplots()
+    ax.bar(top_users, count, color=colour)
+    ax.set_xlabel("Top Users")
+    ax.set_ylabel("Count across runs")
+    ax.set_title(title)
+
     plt.show()
 
 
 if __name__ == "__main__":
-    compare_number_clusters_user("david_madras")
+    # compare_number_clusters_user("david_madras", runs=20)
+    compare_top_users_user("david_madras", runs=20)
+    pass
