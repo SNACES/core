16d9445c183e8a5d9213bd779ac38e108a240cc7
aksh-n <akshatn77@gmail.com>
2022-05-10 06:26:30 +0300

    Add code for the number of top users to graph pairwise following

--- src/clustering_experiments/follow_between_clusters.py
+++ src/clustering_experiments/follow_between_clusters.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from math import prod
 import create_social_graph_and_cluster as csgc
 import matplotlib.pyplot as plt
 from src.shared.utils import get_project_root
@@ -6,10 +7,11 @@ import src.dependencies.injector as sdi
 
 DEFAULT_PATH = str(get_project_root()) + "/src/scripts/config/create_social_graph_and_cluster_config.yaml"
 
-def graph_following_between_clusters(screen_name, c1, c2, c1_name="Cluster 1", c2_name="Cluster 2"):
+def graph_following_between_clusters(screen_name, c1, c2, c1_name="Cluster 1", c2_name="Cluster 2", num_users=-1):
     """Graphs the number of users from cluster 2 are being followed by each user from cluster 1
     sorted by production utility in decreasing order.
     """
+    graph_name = f"./src/clustering_experiments/data/info_flow/{screen_name}_{c1_name}_to_{c2_name}"#_{num_users}"
     initial_user_id = csgc.get_user_by_screen_name(screen_name).id
     injector = sdi.Injector.get_injector_from_file(DEFAULT_PATH)
     process_module = injector.get_process_module()
@@ -25,7 +27,13 @@ def graph_following_between_clusters(screen_name, c1, c2, c1_name="Cluster 1", c
     c2_users = set(c2.users)
     c2_users.remove(str(initial_user_id))
     following_in_c2 = []
-    for user_id in prod_ranking_users:
+
+    if num_users == -1:
+        num_users = len(prod_ranking_users) - 1
+    else:
+        num_users = min(num_users, len(prod_ranking_users) - 1)
+    for i in range(num_users):
+        user_id = prod_ranking_users[i]
         if str(user_id) == str(initial_user_id):
             # so as to ignore spikes, we ignore the initial user
             # as it follows everyone in the local neighbourhood
@@ -44,14 +52,16 @@ def graph_following_between_clusters(screen_name, c1, c2, c1_name="Cluster 1", c
     plt.xlabel(f"Users from {c1_name} sorted by Production Utility")
     plt.ylabel(f"Number of Followers in {c2_name}")
     plt.title(f"Distribution of each user from {c1_name} following users from {c2_name}")
-    plt.savefig(f"./src/clustering_experiments/data/info_flow/{screen_name}_{c1_name}_to_{c2_name}")
+    plt.savefig(graph_name)
     # plt.show()
 
 
-def graph_following_between_clusters_2(screen_name, c1, c2, c1_name="Cluster 1", c2_name="Cluster 2"):
+def graph_following_between_clusters_2(screen_name, c1, c2, c1_name="Cluster 1", c2_name="Cluster 2", num_users=-1):
     """Graphs the number of users from cluster 2 that follow each user from cluster 1
     sorted by production utility in decreasing order.
     """
+    graph_name = f"./src/clustering_experiments/data/info_flow/{screen_name}_{c1_name}_from_{c2_name}"#_{num_users}"
+
     initial_user_id = csgc.get_user_by_screen_name(screen_name).id
     injector = sdi.Injector.get_injector_from_file(DEFAULT_PATH)
     process_module = injector.get_process_module()
@@ -74,7 +84,13 @@ def graph_following_between_clusters_2(screen_name, c1, c2, c1_name="Cluster 1",
                 following_in_c2_dict[u] += 1
     
     following_in_c2 = []
-    for k in prod_ranking_users:
+
+    if num_users == -1:
+        num_users = len(prod_ranking_users) - 1
+    else:
+        num_users = min(num_users, len(prod_ranking_users) - 1)
+    for i in range(num_users):
+        k = prod_ranking_users[i]
         if str(k) == str(initial_user_id):
             # so as to ignore spikes, we ignore the initial user
             # as it follows everyone in the local neighbourhood
@@ -91,18 +107,21 @@ def graph_following_between_clusters_2(screen_name, c1, c2, c1_name="Cluster 1",
     plt.xlabel(f"Users from {c1_name} sorted by Production Utility")
     plt.ylabel(f"Number of Following from {c2_name}")
     plt.title(f"Distribution of {c2_name} users following each user from {c1_name}")
-    plt.savefig(f"./src/clustering_experiments/data/info_flow/{screen_name}_{c1_name}_from_{c2_name}")
+    plt.savefig(graph_name)
     # plt.show()
 
 
-def graph_local_following(screen_name: str, cluster, cluster_name="Cluster X"):
+def graph_local_following(screen_name: str, cluster, cluster_name="Cluster X", prod=True):
     initial_user_id = str(csgc.get_user_by_screen_name(screen_name).id)
     injector = sdi.Injector.get_injector_from_file(DEFAULT_PATH)
     process_module = injector.get_process_module()
     dao_module = injector.get_dao_module()
 
     user_friend_getter = dao_module.get_user_friend_getter()
-    prod_ranker = process_module.get_ranker()
+    if prod:
+        prod_ranker = process_module.get_ranker()
+    else:
+        prod_ranker = process_module.get_ranker("Consumption")
     
     prod_ranking, _ = prod_ranker.rank(initial_user_id, cluster)
     # List of users in c1 sorted by production utility in descending order
@@ -112,7 +131,7 @@ def graph_local_following(screen_name: str, cluster, cluster_name="Cluster X"):
     local_following = []
 
     for curr_user in prod_ranking_users:
-        if curr_user == str(user_id):
+        if curr_user == str(initial_user_id):
             continue
         global_friends = {str(u) for u in user_friend_getter.get_user_friends_ids(curr_user)}
         local_friends_count = len(global_friends.intersection(cluster_users))
@@ -123,24 +142,34 @@ def graph_local_following(screen_name: str, cluster, cluster_name="Cluster X"):
 
     fig, ax = plt.subplots()
     ax.plot(x_vals, y_vals, label="cluster following list", color="C0")
-    ax.set_ylim([0, len(cluster_users) - 1])
+    ax.set_ylim([0, len(cluster_users)])
     # plt.plot(x_vals, [overlap_threshold for _ in range(N - 1)], label="local following list threhold")
 
-    plt.xlabel("Users sorted by Production Utility")
     plt.ylabel("Number of Following in Cluster")
     plt.legend()
     plt.title(f"Distribution of Following for User -- {screen_name}, {cluster_name}")
-    plt.savefig(f"./src/clustering_experiments/data/info_flow/clusterfollowing_for_{screen_name}_{cluster_name}.png")
+    graph_name = f"./src/clustering_experiments/data/info_flow/clusterfollowing_for_{screen_name}_{cluster_name}"
+    if prod:
+        plt.xlabel("Users sorted by Production Utility")
+        graph_name = graph_name + ".png"
+        plt.savefig(graph_name)
+    else:
+        plt.xlabel("Users sorted by Consumption Utility")
+        graph_name = graph_name + "_cons" + ".png"
+        plt.savefig(graph_name)
 
 
-def graph_local_follower(screen_name: str, cluster, cluster_name="Cluster X"):
+def graph_local_follower(screen_name: str, cluster, cluster_name="Cluster X", prod=True):
     initial_user_id = csgc.get_user_by_screen_name(screen_name).id
     injector = sdi.Injector.get_injector_from_file(DEFAULT_PATH)
     process_module = injector.get_process_module()
     dao_module = injector.get_dao_module()
 
     user_friend_getter = dao_module.get_user_friend_getter()
-    prod_ranker = process_module.get_ranker()
+    if prod:
+        prod_ranker = process_module.get_ranker()
+    else:
+        prod_ranker = process_module.get_ranker("Consumption")
     
     prod_ranking, _ = prod_ranker.rank(initial_user_id, cluster)
     # List of users in c1 sorted by production utility in descending order
@@ -173,18 +202,25 @@ def graph_local_follower(screen_name: str, cluster, cluster_name="Cluster X"):
 
     fig, ax = plt.subplots()
     ax.plot(x_vals, y_vals, label="cluster follower list", color="C2")
-    ax.set_ylim([0, len(cluster_users) - 1])
+    ax.set_ylim([0, len(cluster_users)])
     # plt.plot(x_vals, [overlap_threshold for _ in range(N - 1)], label="local following list threhold")
 
-    plt.xlabel("Users sorted by Production Utility")
     plt.ylabel("Number of Followers in Cluster")
     plt.legend()
     plt.title(f"Distribution of Followers for User -- {screen_name}, {cluster_name}")
-    plt.savefig(f"./src/clustering_experiments/data/info_flow/clusterfollower_for_{screen_name}_{cluster_name}.png")
+    graph_name = f"./src/clustering_experiments/data/info_flow/clusterfollower_for_{screen_name}_{cluster_name}"
+    if prod:
+        plt.xlabel("Users sorted by Production Utility")
+        graph_name = graph_name + ".png"
+        plt.savefig(graph_name)
+    else:
+        plt.xlabel("Users sorted by Consumption Utility")
+        graph_name = graph_name + "_cons" + ".png"
+        plt.savefig(graph_name)
 
 
 
-def graph_size_clusters(screen_name: str, clusters: list):
+def graph_size_clusters(screen_name: str, clusters: list, threshold=0.3, suffix=None):
     """Graphs the clusters based on the size of clusters.
     Does not graph clusters below the discard_size.
     """
@@ -194,11 +230,14 @@ def graph_size_clusters(screen_name: str, clusters: list):
     y_vals = [c[1] for c in count_pairs]
     x_vals = [str(c[0]) for c in count_pairs]
     ax.bar(x_vals, y_vals)
-    plt.title("Number of Clusters for Each Size, threshold=0.3")
+    plt.title(f"Number of Clusters for Each Size, threshold={threshold}")
     plt.ylabel("Count of Clusters of Each Size")
     plt.xlabel("Size of clusters")
 
-    plt.savefig(f"./src/clustering_experiments/data/info_flow/{screen_name}_size_clusters")
+    if suffix is None:
+        plt.savefig(f"./src/clustering_experiments/data/info_flow/{screen_name}_{threshold}_size_clusters.png")
+    else:
+        plt.savefig(f"./src/clustering_experiments/data/info_flow/{screen_name}_{threshold}_size_clusters_{suffix}.png")
     # plt.show()
 
 
@@ -212,13 +251,15 @@ if __name__ == "__main__":
     sorted_clusters = sorted(refined_clusters, key=lambda c: len(c.users), reverse=True)
 
     # graph_size_clusters(screen_name, sorted_clusters)
-    # for i in range(5):
-    #     for j in range(i + 1, 5):
-    #         # graph_following_between_clusters(screen_name, sorted_clusters[i], sorted_clusters[j], f"Cluster {i}", f"Cluster {j}")
-    #         # graph_following_between_clusters(screen_name, sorted_clusters[j], sorted_clusters[i], f"Cluster {j}", f"Cluster {i}")
-    #         graph_following_between_clusters_2(screen_name, sorted_clusters[i], sorted_clusters[j], f"Cluster {i}", f"Cluster {j}")
-    #         graph_following_between_clusters_2(screen_name, sorted_clusters[j], sorted_clusters[i], f"Cluster {j}", f"Cluster {i}")
+    for i in range(7):
+        for j in range(i + 1, 7):
+            graph_following_between_clusters(screen_name, sorted_clusters[i], sorted_clusters[j], f"Cluster {i}", f"Cluster {j}")# , num_users=10)
+            graph_following_between_clusters(screen_name, sorted_clusters[j], sorted_clusters[i], f"Cluster {j}", f"Cluster {i}")#, num_users=10)
+            graph_following_between_clusters_2(screen_name, sorted_clusters[i], sorted_clusters[j], f"Cluster {i}", f"Cluster {j}")#, num_users=10)
+            graph_following_between_clusters_2(screen_name, sorted_clusters[j], sorted_clusters[i], f"Cluster {j}", f"Cluster {i}")#, num_users=10)
 
-    for i in range(5):
-        graph_local_following(screen_name, sorted_clusters[i], f"Cluster {i}")
-        graph_local_follower(screen_name, sorted_clusters[i], f"Cluster {i}")
+    for i in range(7):
+        graph_local_following(screen_name, sorted_clusters[i], f"Cluster {i}", prod=True)
+        graph_local_follower(screen_name, sorted_clusters[i], f"Cluster {i}", prod=True)
+        graph_local_following(screen_name, sorted_clusters[i], f"Cluster {i}", prod=False)
+        graph_local_follower(screen_name, sorted_clusters[i], f"Cluster {i}", prod=False)
